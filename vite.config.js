629f77a7f50a262dae596a366d21b85b41134da2
saurabh saini <suryasaini427@gmail.com>
2024-09-21 04:26:09 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+ import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,  // This allows access to 0.0.0.0
+    port: process.env.PORT || 3000, // Render's PORT will be used
+  },
+  build: {
+    outDir: 'build', // Set the output directory for static files
+  },
+});
